6b2e896f8c4d499a46374b48387be1ea868e6b72
SnailDOS <zarasoonich@hotmail.com>
2022-10-08 14:45:37 +0800

    finish make

---
+++ make/pre_install.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+import os 
+
+print("Generating Directories...")
+
+if not os.path.exists("/opt/com.encryptos.editor"):
+    os.makedirs("/opt/com.encryptos.editor/icons")
